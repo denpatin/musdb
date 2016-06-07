16f946c43bd4e6c29b00bfbe4208d1ad0e79376c
Den Patin <denpatin@gmail.com>
2016-06-07 19:28:59 +0300

    Initial commit

---
+++ tracks.js
@@ -0,0 +1,65 @@
+var movies = [
+  {
+    "name": "The Shawshank Redemption",
+    "outline": "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+    "rating": 9.3,
+    "director": "Frank Darabont",
+    "year": 1994,
+    "stars": [
+      "Tim Robbins",
+      "Morgan Freeman",
+      "Bob Gunton"
+    ],
+    "runtime": 142,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "certificate": "R",
+    "date": "1974-04-30T10:29:29+05:30",
+    "actor": "Tim Robbins",
+    "id": 1
+  },
+  {
+    "name": "The Godfather",
+    "outline": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+    "rating": 9.2,
+    "director": "Francis Ford Coppola",
+    "year": 1972,
+    "stars": [
+      "Marlon Brando",
+      "Al Pacino",
+      "James Caan"
+    ],
+    "runtime": 175,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "certificate": "R",
+    "date": "1974-07-12T10:29:29+05:30",
+    "actor": "Marlon Brando",
+    "id": 2
+  },
+  {
+    "name": "The Godfather: Part II",
+    "outline": "The early life and career of Vito Corleone in 1920s New York is portrayed while his son, Michael, expands and tightens his grip on his crime syndicate stretching from Lake Tahoe, Nevada to pre-revolution 1958 Cuba.",
+    "rating": 9.1,
+    "director": "Francis Ford Coppola",
+    "year": 1974,
+    "stars": [
+      "Al Pacino",
+      "Robert De Niro",
+      "Robert Duvall"
+    ],
+    "runtime": 200,
+    "genre": [
+      "Crime",
+      "Drama"
+    ],
+    "certificate": "R",
+    "date": "1992-08-23T10:29:29+05:30",
+    "actor": "Al Pacino",
+    "id": 3
+  }
+]
